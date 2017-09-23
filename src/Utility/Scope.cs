f17c388e026712abb280b7fa2202d220ef31479f
Chris Culy <chris@chrisculy.com>
2017-09-22 21:15:42 -0700

    Add generic Scope class that allows executing code at the end of a using block.

---
+++ src/Utility/Scope.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Epoxy.Utility
+{
+    public class Scope : IDisposable
+    {
+        public Scope(Action onDispose)
+        {
+            m_onDispose = onDispose;
+        }
+
+        public void Dispose()
+        {
+            m_onDispose();
+        }
+
+        private Action m_onDispose;
+    }
+}
